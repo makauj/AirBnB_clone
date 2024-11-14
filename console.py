83223564c423707ef5db6c170ff6400dcdc44831
makau <makauwanyoike@gmail.com>
2024-11-14 14:37:57 +0300

    pycodestyle

--- console.py
+++ console.py
@@ -9,33 +9,33 @@ from models.base_model import BaseModel
 
 class HBNBCommand(cmd.Cmd):
     """command line interpreter"""
-    
+
     prompt = '(hbnb)'
-    
+
     def quit(self, arg):
         """quit the command line interpreter"""
         return True
-    
+
     def do_EOF(self, arg):
         """Exit cmd on EOF"""
         return True
-    
+
     def do_help(self, arg):
         """Custom help for AirBnB project"""
         super().do_help(arg)
-    
+
     def emptylibe(self):
         """Override emptyline method to prevent execution of empty commands"""
         pass
-    
+
     def default(self, arg):
         """Handle default case when an unknown command is entered"""
         print(f"*** Unknown syntax: {arg}")
-    
+
     def create(self, arg):
         """Create a new instance of BaseModel"""
         commands  = shlex.splir(arg)
-        
+
         if len(commands) == 0:
             print(f"*** class name missing")
         elif commands[0] not in self.valid_classes:
