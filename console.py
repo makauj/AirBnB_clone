52956e630580bc16e43ba8c2e24b46b13655ec1f
makau <makauwanyoike@gmail.com>
2024-11-14 14:06:34 +0300

    more console stuff

--- console.py
+++ console.py
@@ -2,7 +2,10 @@
 """entry point of the command interpreter"""
 
 import cmd
-import os
+from cmd import Cmd
+import shlex
+from models import storage
+from models.base_model import BaseModel
 
 
 class HBNBCommand(cmd, Cmd):
@@ -29,7 +32,19 @@ class HBNBCommand(cmd, Cmd):
     def default(self, arg):
         """Handle default case when an unknown command is entered"""
         print(f"*** Unknown syntax: {arg}")
+    
+    def create(self, arg):
+        """Create a new instance of BaseModel"""
+        commands  = shlex.splir(arg)
         
+        if len(commands) == 0:
+            print(f"*** class name missing")
+        elif commands[0] not in self.valid_classes:
+            print("*** class doesn't exist")
+        else:
+            new_instance = eval(f"{commands[0]}()")
+            storage.save()
+            print(new_instance.id)
 
 
 
